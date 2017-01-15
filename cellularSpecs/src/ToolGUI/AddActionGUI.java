e9cf693cc159c00fc89e367cad6f4846f609e081
Ahmad Mnasra <Ahmad Mnasra>
2017-01-15 18:16:31 +0200

    add new pic ,, finish UML

--- cellularSpecs/src/ToolGUI/AddActionGUI.java
+++ cellularSpecs/src/ToolGUI/AddActionGUI.java
@@ -25,82 +25,80 @@ public class AddActionGUI extends JFrame
 		getContentPane().setLayout(null);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"wifi"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"<dynamic>"}));
 		comboBox.setEnabled(false);
-		comboBox.setBounds(108, 44, 84, 20);
+		comboBox.setBounds(116, 83, 84, 20);
 		getContentPane().add(comboBox);
 		
-		JLabel lblChooseParam = new JLabel("Choose param");
-		lblChooseParam.setBounds(42, 113, 84, 14);
+		JLabel lblChooseParam = new JLabel("Choose param:\r\n");
+		lblChooseParam.setBounds(42, 152, 84, 14);
 		getContentPane().add(lblChooseParam);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(118, 110, 89, 20);
+		comboBox_1.setBounds(118, 149, 79, 20);
 		getContentPane().add(comboBox_1);
 		
 		JLabel label_2 = new JLabel("Change VAL:");
-		label_2.setBounds(248, 113, 69, 14);
+		label_2.setBounds(207, 152, 69, 14);
 		getContentPane().add(label_2);
 		
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(315, 110, 78, 20);
+		comboBox_2.setBounds(273, 149, 78, 20);
 		getContentPane().add(comboBox_2);
 		
-		JLabel lblOnoff = new JLabel("<dynamic>- ONOFF - Add Action");
+		JLabel lblOnoff = new JLabel("<dynamic>- ON/OFF - Add Action");
 		lblOnoff.setFont(new Font("Arial", Font.BOLD, 22));
-		lblOnoff.setBounds(32, 0, 361, 36);
+		lblOnoff.setBounds(22, 21, 361, 36);
 		getContentPane().add(lblOnoff);
 		
 		JLabel lblNewLabel = new JLabel("Condition for action: ");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel.setBounds(22, 177, 116, 20);
+		lblNewLabel.setBounds(10, 216, 116, 20);
 		getContentPane().add(lblNewLabel);
 		ImageIcon imageForOne = new ImageIcon(getClass().getResource("../delete.png"));
 		JButton btnAddOtherConditon = new JButton("+");
-		btnAddOtherConditon.setBounds(146, 177, 41, 21);
+		btnAddOtherConditon.setBounds(502, 216, 41, 21);
 		getContentPane().add(btnAddOtherConditon);
 		
 		JLabel lblAction = new JLabel("Action:");
 		lblAction.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblAction.setBounds(10, 76, 63, 20);
+		lblAction.setBounds(10, 121, 63, 20);
 		getContentPane().add(lblAction);
 		
-		JLabel label = new JLabel("Choose param");
-		label.setBounds(27, 47, 84, 14);
-		getContentPane().add(label);
+		JLabel lblChooseParam_1 = new JLabel("Choose param:");
+		lblChooseParam_1.setBounds(35, 86, 84, 14);
+		getContentPane().add(lblChooseParam_1);
 		
 		JLabel lblValue = new JLabel("Value=");
-		lblValue.setBounds(218, 47, 84, 14);
+		lblValue.setBounds(231, 86, 41, 14);
 		getContentPane().add(lblValue);
 		
 		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"ON", "OFF"}));
-		comboBox_3.setBounds(262, 44, 84, 20);
+		comboBox_3.setBounds(267, 83, 84, 20);
 		getContentPane().add(comboBox_3);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 156, 499, 20);
+		separator.setBounds(22, 213, 550, 20);
 		getContentPane().add(separator);
 		
-		JLabel label_1 = new JLabel("Choose param");
-		label_1.setBounds(56, 212, 84, 14);
-		getContentPane().add(label_1);
+		JLabel lblChooseParam_2 = new JLabel("Choose param:");
+		lblChooseParam_2.setBounds(42, 247, 84, 14);
+		getContentPane().add(lblChooseParam_2);
 		
 		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setEnabled(false);
-		comboBox_4.setBounds(139, 209, 84, 20);
+		comboBox_4.setBounds(116, 244, 79, 20);
 		getContentPane().add(comboBox_4);
 		
 		JLabel label_3 = new JLabel("Value=");
-		label_3.setBounds(233, 211, 84, 14);
+		label_3.setBounds(231, 244, 63, 14);
 		getContentPane().add(label_3);
 		
 		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setBounds(277, 208, 84, 20);
+		comboBox_5.setBounds(267, 244, 84, 20);
 		getContentPane().add(comboBox_5);
 		
 		JButton button = new JButton("+");
-		button.setBounds(118, 75, 41, 21);
+		button.setBounds(502, 121, 41, 21);
 		getContentPane().add(button);
 		
 		JButton btnSave = new JButton("Save");
@@ -112,11 +110,15 @@ public class AddActionGUI extends JFrame
 		getContentPane().add(button_2);
 		
 		JLabel lblNewLabel_1 = new JLabel(imageForOne);
-		lblNewLabel_1.setBounds(32, 212, 25, 14);
+		lblNewLabel_1.setBounds(22, 247, 25, 14);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel label_4 = new JLabel(imageForOne);
-		label_4.setBounds(22, 113, 25, 14);
+		label_4.setBounds(22, 152, 25, 14);
 		getContentPane().add(label_4);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(22, 114, 550, 20);
+		getContentPane().add(separator_1);
 	}
 }
